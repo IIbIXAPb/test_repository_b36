597d65e93c11ef623a2bf544d65e71d5391e7918
Pavel <iibixapb@gmail.com>
2023-02-03 00:35:01 +0300

    Update test_example.py
    New work

--- Work_1/3/python-example/test_example.py
+++ Work_1/3/python-example/test_example.py
@@ -13,8 +13,9 @@ def driver(request):
     return wd
 
 def test_example(driver):
-    driver.get("http://localhost/litecart/en/")
-    driver.find_element(By.NAME, "email").send_keys("admin")
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element(By.NAME, "username").send_keys("admin")
     driver.find_element(By.NAME, "password").send_keys("admin")
     driver.find_element(By.NAME, "login").click()
-    WebDriverWait(driver, 10).until(EC.title_is("Login | My Store"))
+    WebDriverWait(driver, 10).until(EC.title_is("My Store"))
+
