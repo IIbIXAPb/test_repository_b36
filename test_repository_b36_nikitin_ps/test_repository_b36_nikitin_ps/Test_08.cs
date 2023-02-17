8c6290ba05bcb9377fd60e23264fb3280eacb7d1
Pavel <iibixapb@gmail.com>
2023-02-18 00:04:22 +0300

    New project

---
+++ test_repository_b36_nikitin_ps/test_repository_b36_nikitin_ps/Test_08.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_repository_b36_nikitin_ps
+{
+    internal class Test_08
+    {
+    }
+}
